be39c2bb66bc44d8ff5c84e765dd628816a2445a
RANJAN KUMAR SAH <60313003+AakashRanjansh@users.noreply.github.com>
2022-08-23 11:45:25 +0530

    uploaded Updated Project

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
-from time import sleep
 import cvzone
 import os
 import time
 import pyautogui
 import screen_brightness_control as sbc
-
+import speech_recognition as sr
+from pywhatkit.core import core
+import webbrowser as web
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 1280)
 cap.set(4, 720)
 
-detector = HandDetector(detectionCon=0.8)
+detector = HandDetector(detectionCon=0.8,maxHands=1)
 keys = [["Chrome", "Notepad", "SwApp"],
         ["?sapp", "Librewlf", "Sound+"],
         ["Bright-", "Bright+", "Sound-"]]
 finalText = ""
 
-
 def drawAll(img, buttonList):
     for button in buttonList:
         x, y = button.pos
         w, h = button.size
-        # print(x, y)
         cvzone.cornerRect(img, (button.pos[0], button.pos[1], button.size[0], button.size[1]),
                           20, rt=0)
         cv2.rectangle(img, button.pos, (x + w, y + h), (0, 100, 240), cv2.FILLED)
         cv2.putText(img, button.text, (x + 20, y + 65),
                     cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+        # icon = cv2.imread('download.png')
+        # size = 100
+        # icon = cv2.resize(icon,(size,size))
+
     return img
 
 
@@ -44,7 +47,7 @@ for i in range(len(keys)):
     for j, key in enumerate(keys[i]):
         buttonList.append(Button([450 * j + 50, 200 * i + 50], key))  # left increase Horizontal size
 
-while True:
+while cap.isOpened():
     success, in_img = cap.read()
     img = cv2.flip(in_img, 1)
 
@@ -61,7 +64,8 @@ while True:
             w, h = button.size
 
             if x < lmList[8][0] < x + w and y < lmList[8][1] < y + h:
-                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5), (175, 0, 175), cv2.FILLED)
+                cv2.rectangle(img, (x - 5, y - 5), (x + w + 5, y + h + 5),
+                              (175, 0, 175), cv2.FILLED)
                 cv2.putText(img, button.text, (x + 20, y + 65),
                             cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
                 l, _, _ = detector.findDistance(8, 12, img, draw=False)
@@ -78,10 +82,10 @@ while True:
                     if aakashkey1 == 'Chrome':
                         command = "Start chrome"
                         os.system(command)
-                        sleep(3)
+                        time.sleep(3)
                         pyautogui.write('youtube.com', interval=0.05)
                         pyautogui.press('enter')
-                        sleep(3)
+                        time.sleep(3)
                         time.sleep(5)
                         pyautogui.hotkey('win', 'down')
 
@@ -91,25 +95,18 @@ while True:
                         time.sleep(1)
                         pyautogui.hotkey('ctrl', 'n')
 
-                        pyautogui.write('Hello forbes how are you , this is the Partial project of Ranjan kumar'
-                                        '\nif (WantToSee == FullProject):\n\n\tPlease wait till Final Review.\n\n',
+                        pyautogui.write('Hello folks how are you, this is the Ongoing project of Ranjan kumar'
+                                        '\nif (WantToSee == FullProject):\n\n\tPlease wait till completion.\n\n',
                                         interval=0.05)
                         pyautogui.press('enter')
                         pyautogui.press('backspace', presses=3)
                         pyautogui.write('\nelse:\n\n\tNo way to see,you cant find AnyWhere.', interval=0.05)
-                        # pyautogui.press('volumedown')
                         time.sleep(0.1)
-                        # sleep(3)
                         time.sleep(5)
                         pyautogui.hotkey('win', 'down')
 
                     elif aakashkey1 == 'SwApp':
-                        # command = "start msedge"
-                        # os.system(command)
-                        # time.sleep(5)
-                        # pyautogui.hotkey('win', 'down')
                         pyautogui.keyDown('alt')
-                        # time.sleep(1)
                         pyautogui.press('tab', )
                         time.sleep(0.1)
                         pyautogui.press('tab', )
@@ -125,23 +122,44 @@ while True:
                     #     pyautogui.hotkey('win', 'down')
 
                     elif aakashkey1 == "?sapp":
-                        pyautogui.hotkey('win', 'r')
+                        web.open("https://web.whatsapp.com/send?phone=+9779825830731")
+                        time.sleep(7)
+                        pyautogui.click(core.WIDTH / 2, core.HEIGHT / 2)
+                        time.sleep(3)
+                        pyautogui.click()
                         time.sleep(0.5)
-                        pyautogui.typewrite('https://web.whatsapp.com/send?phone=+9779825830731')
-                        pyautogui.press('enter')
-                        time.sleep(10)
-                        pyautogui.press('tab', presses=10, interval=0.1)
 
-                        pyautogui.typewrite('Hello Aakash This is Auto Typed Message', interval=0.1)
+
+                        pyautogui.typewrite('Hello Aakash This is Auto Typed Message', interval=0.05)
                         pyautogui.press('enter')
                         time.sleep(1)
-                        pyautogui.typewrite('This is again second Auto Typed Message, dont want to Spam so,',
+                        pyautogui.typewrite('Now This is Switching To Manual Voice Typing Mode',
                                             interval=0.1)
                         pyautogui.press('enter')
                         time.sleep(1)
-                        pyautogui.typewrite('BYE', interval=0.1)
-                        pyautogui.press('enter')
-                        time.sleep(5)
+                        r = sr.Recognizer()
+
+                        # listening the speech and store in audio_text variable
+                        while True:
+                            with sr.Microphone() as source:
+                                audio_text = r.listen(source)
+
+                                try:
+                                    SPtotext = r.recognize_google(audio_text)
+                                except:
+                                    print("Sorry, I did not get that")
+                                    break
+
+                            Aakash = str(SPtotext)
+                            Ranjan = Aakash.lower()
+                            print(SPtotext)
+                            if Ranjan == "exit":
+                                break
+
+                            pyautogui.typewrite(SPtotext, interval=0.1)
+                            pyautogui.press('enter')
+
+                        time.sleep(1)
                         pyautogui.hotkey('win', 'down')
 
                     elif aakashkey1 == "Sound+":
@@ -165,7 +183,10 @@ while True:
                                         button='OK')
                         # print("No input")
 
-                    sleep(0.15)
+                    time.sleep(0.15)
 
     cv2.imshow("GESTURE SYSTEM CONTROL", img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
